0993f3ac9e0b645bb658487c706fd59e1a3fc8ec
kevin trentini <trentinikevin6@gmail.com>
2021-03-10 15:16:12 +0100

    Add project files.

---
+++ EsercitazioneLib/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EsercitazioneLib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ EsercitazioneLib_Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EsercitazioneLib_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
